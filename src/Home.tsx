64bd9573bf42e3ab76ba30c538b3151ee4574246
auberginearms <a.mia.oh@gmail.com>
2023-03-06 22:14:36 +0800

    fetch home text from backend

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,17 @@
 import { ReactElement } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_HOME } from "./gql/query";
 
 export function Home(): ReactElement {
+  const { loading, error, data } = useQuery(GET_HOME);
   return (
     <div>
       <img src="https://media.istockphoto.com/id/1267041706/vector/hand-drawn-mushrooms-banner.jpg?s=170667a&w=0&k=20&c=hXenDpCIHQruvRPpGUGhRX0lKvQz7nx_w2q5V0jssMg="></img>
       <h1 style={{ display: "flex", justifyContent: "center" }}>
-        Welcome to Shroomland
+        {data?.home.pageHeader}
       </h1>
       <p style={{ display: "flex", justifyContent: "center" }}>
-        Prepare to be shroomed
+        {data?.home.pageDescription}
       </p>
     </div>
   );
